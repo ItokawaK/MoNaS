7a0873a0144fcea58fd62685f68dd534fc911854
itokawa <itokawa@nih.go.jp>
2018-12-17 18:14:53 +0900

    Refined code

--- scripts/finalize_table.py
+++ scripts/finalize_table.py
@@ -20,8 +20,8 @@ class VCF_info:
                     continue
                 if(info_list[0].startswith("@")):
                     continue
-                res = re.search(r"(..)$", info_list[2])
-                splice_variant = res.group(1)
+                #res = re.search(r"(..)$", info_list[2])
+                splice_variant = info_list[2][-2:] #res.group(1)
                 self.dict[splice_variant] = info_list
                 self.allele = info_list
 
