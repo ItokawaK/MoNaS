c3fa6b35da95e8d1ee433e2ff77ee9ab5da83815
itokawa <itokawa@nih.go.jp>
2019-01-21 13:10:29 +0900

    refined code

--- scripts/finalize_table.py
+++ scripts/finalize_table.py
@@ -146,43 +146,53 @@ class MDom_comvert:
             self.kdr_dict = json.load(f)
 
     def fasta_2_hash(self, fasta):
-        Mdom_AA_aligned = []
-        Mos_AA_aligned = []
+        """
+        Creating [mosquito AA pos] -> [M. domestica AA pos] hash function from
+        multiple aligment fasta. The first entry in the fasta will be regarded
+        as M. domestica AA seq. Currently, the third and follwing sequences will
+        be ignored because ck dl transcript variants will have exactly same
+        aligment to M. domestica VGSC.
+        """
 
         with open(fasta) as f:
+            AA_seqs = []
+
             if f.readline().startswith(">"):
-                is_Mdom = True
+                seq_buf = []
             else:
                 print(fasta + ' is not in valid format!', file = sys.stderr)
                 sys.exit(1)
+
             for line in f.readlines():
                 if line.startswith(">"):
-                    is_Mdom = False
-                    continue
-                if is_Mdom:
-                    Mdom_AA_aligned += list(line.rstrip())
+                    AA_seqs.append(seq_buf)
+                    seq_buf = []
                 else:
-                    Mos_AA_aligned += list(line.rstrip())
+                    seq_buf += list(line.rstrip())
 
-        if not len(Mdom_AA_aligned) == len(Mos_AA_aligned):
+            AA_seqs.append(seq_buf)
+
+
+
+        if not len(AA_seqs[0]) == len(AA_seqs[1]):
             raise ValueError("error!")
 
         idx = [0, 0]
 
-        for mos, mdom in zip(Mos_AA_aligned, Mdom_AA_aligned):
+        for mdom, mos in zip(AA_seqs[0], AA_seqs[1]):
             if mos != "-":
                 idx[0] += 1
-                a = str(idx[0])
+                mos_no = str(idx[0])
             else:
-                a = "-"
+                mos_no = "-"
             if mdom != "-":
                 idx[1] += 1
-                b = str(idx[1])
+                mdom_no = str(idx[1])
             else:
-                b = "-"
+                mdom_no = "-"
 
-            if a != "-":
-                self.hash[a] = b
+            if mos != "-":
+                self.hash[mos_no] = mdom_no
 
             if debug:
                 print("\t".join([str(idx[0]), mos, mdom, str(idx[1])]), file = sys.stderr)
@@ -210,11 +220,12 @@ class MDom_comvert:
 
 class Bed:
     class Exon:
-        def __init__(self, name, start , end):
+        def __init__(self, start , end, name, strand):
             self.name = name
             self.no = int(re.search(pattern= "\d+", string= name).group())
             self.start = int(start)
             self.end = int(end)
+            self.strand = strand
             if name[-1] in ('c', 'k', 'd', 'l'):
                self.mut_excl = name[-1]
             else:
@@ -225,16 +236,35 @@ class Bed:
         with open(bed_file) as f:
             bed_lines = [l.rstrip() for l in f.readlines() if not l.startswith("#")]
 
-        entries = []
+        self.entries = []
+        self.headers = []
         for line in bed_lines:
-            self.chrom, start, end, name = line.split("\t")
-            entries.append(self.Exon(name, start, end))
-        self.entries = entries
-        if (self.entries[1].no < self.entries[2].no) == \
-                            (self.entries[1].start < self.entries[2].end):
-            self.strand = "+"
+            last_strand = None
+            if line.startswith('browser') or  line.startswith('track'):
+                self.headers.append(line.split("\t"))
+            else:
+                tmp = line.split("\t")
+                self.chrom = tmp[0]
+                if len(tmp) > 5:
+                    self.entries.append(self.Exon(tmp[1], tmp[2], tmp[3], tmp[5]))
+                    fix_strand = False
+                    if last_strand and last_strand != tmp[5]:
+                        sys.exit("Error!: There are two different strands in " + bed_file)
+                    last_strand = tmp[5]
+                else:
+                    self.entries.append(self.Exon(tmp[1], tmp[2], tmp[3], "+"))
+                    fix_strand = True
+
+        if fix_strand:
+            if (self.entries[0].no < self.entries[1].no) == \
+                                (self.entries[0].start < self.entries[1].end):
+                self.strand = "+"
+            else:
+                self.strand = "-"
+            for exon in self.entries:
+                exon.strand = self.strand
         else:
-            self.strand = "-"
+            self.strand = self.entries[0].strand
 
     def which_exon(self, pos):
             for Exon in self.entries:
